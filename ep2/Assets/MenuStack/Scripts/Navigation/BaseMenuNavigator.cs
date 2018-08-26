9a361ca57138e40777ab1f50a4563107ab2027d5
bengreenier <ben@bengreenier.com>
2018-08-26 15:48:45 -0400

    ep2 content

---
+++ ep2/Assets/MenuStack/Scripts/Navigation/BaseMenuNavigator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEditor.Events;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace MenuStack.Navigation
+{
+    /// <summary>
+    /// Base for a control capable of navigating to a different <see cref="Menu"/> within a <see cref="MenuRoot"/>
+    /// </summary>
+    public abstract class BaseMenuNavigator : MonoBehaviour, INavigator
+    {
+        /// <summary>
+        /// Provides a location to navigate to, when navigation occurs
+        /// </summary>
+        /// <returns><see cref="Menu"/> to navigate to</returns>
+        protected abstract Menu GetNavigationLocation();
+
+        /// <summary>
+        /// Triggers navigation
+        /// </summary>
+        public virtual void Navigate()
+        {
+            this.GetComponentInParent<MenuRoot>().OpenAsync(this.GetNavigationLocation());
+        }
+    }
+}
